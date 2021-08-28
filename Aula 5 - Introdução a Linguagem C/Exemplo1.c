5df87b5feff5d6ea2b6a942132cd558f46063da0
profdiegomoraeseduvale <80609774+profdiegomoraeseduvale@users.noreply.github.com>
2021-08-28 14:23:34 -0300

    feat: Commit Aula 5
    Exemplos introdutórios para a linguagem C

---
+++ Aula 5 - Introdução a Linguagem C/Exemplo1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+
+    char nome[80];
+
+	
+	printf("\n Digite seu nome: ");
+	scanf("%s",&nome);
+
+	printf("O nome é %s\n ",nome);
+
+}
---
+++ Aula 5 - Introdução a Linguagem C/Exemplo3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+	
+
+    float num1, num2, resultado;
+
+    printf("Digite o primeiro número ");
+    scanf("%f", &num1);
+
+    printf("Digite o segundo número ");
+    scanf("%f", &num2);
+
+    resultado = num1/num2;
+
+    printf("O resultado é %.2f",resultado);
+
+	
+}
